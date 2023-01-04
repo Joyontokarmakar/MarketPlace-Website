dd23676fceee7382d85b2281b1abf2e1b9bcd539
Joyonto Karmakar <joyonto1997@gmail.com>
2023-01-05 02:21:37 +0600

    till top collection section done

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,236 @@
 <template>
-     <div>
-          <h1>Hello work space</h1>
+     <!-- Banner Start -->
+     <div class="commonSpacing flex justify-start items-start py-[60px]">
+          <div class="w-[55%]">
+               <h2 class="text-[40px] leading-[48px] font-black">Discover, and collect Digital Art  NFTs </h2>
+               <p class="text-2xl text-primaryText mt-[22px]">Digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, Sell, and discover exclusive digital assets.</p>
+               <button class="mt-[41px] bg-primaryBlue text-white text-sm font-bold border-primaryBlue border-2 rounded-full px-4 h-[50px]">Explore Now</button>
+               <div class="flex justify-start gap-x-4 mt-[30px]">
+                    <div>
+                         <h2 class="text-[40px] leading-[48px] font-black">98k+</h2>
+                         <p class="text-2xl text-primaryText">Artwork</p>
+                    </div>
+                    <div>
+                         <h2 class="text-[40px] leading-[48px] font-black">12k+</h2>
+                         <p class="text-2xl text-primaryText">Auction</p>
+                    </div>
+                    <div>
+                         <h2 class="text-[40px] leading-[48px] font-black">15k+</h2>
+                         <p class="text-2xl text-primaryText">Artist</p>
+                    </div>
+               </div>
+          </div>
+          <div class="w-[45%] h-full pl-10">
+               <div class="relative w-full h-full">
+                    <!-- banner header -->
+                    <div class="absolute left-5 top-1 z-30 h-20 w-11/12 px-5 py-4 text-white">
+                         <p class="text-3xl font-bold">Abstr Gradient NFT</p>
+                         <div class="mt-2 flex items-center justify-start gap-x-3">
+                              <img class="w-8" src="@/assets/images/avatar/humanAvatar-1.svg" alt="" />
+                              <p class="text-xl font-bold">Arkhan17</p>
+                         </div>
+                    </div>
+                    <img class="absolute left-0 z-20 scale-100 cursor-pointer rounded-3xl transition duration-300 ease-in hover:-translate-y-1 hover:scale-105" src="@/assets/images/cardsimg/heroBannerImg-1.svg" alt="" />
+                    <img class="absolute left-20 z-10 scale-90 cursor-pointer rounded-3xl transition duration-300 ease-in hover:-translate-y-1 hover:scale-105" src="@/assets/images/cardsimg/heroBannerImg-2.svg" alt="" />
+                    <img class="absolute left-40 z-0 scale-75 cursor-pointer rounded-3xl transition duration-300 ease-in hover:-translate-y-1 hover:scale-105" src="@/assets/images/cardsimg/heroBannerImg-3.svg" alt="" />
+                    <img class="absolute -left-20 top-44 z-30 w-40" src="@/assets/images/cardsimg/heroBannerHolmark.svg" alt="" />
+                    <!-- banner footer -->
+                    <div class="absolute left-5 top-[21rem] z-30 flex h-20 w-[70%] justify-between rounded-xl bg-white bg-opacity-20 px-8 py-4 text-white backdrop-blur-xl">
+                         <div class="w-1/2">
+                              <p class="text-base font-medium leading-4">Current Bid</p>
+                              <div class="flex items-center justify-start gap-x-2">
+                                   <img class="w-2" src="@/assets/images/icons/whiteRombosIcon.svg" alt="" />
+                                   <h2 class="text-lg font-bold">0.25 ETH</h2>
+                              </div>
+                         </div>
+                         <div class="w-1/2">
+                              <p class="text-base font-medium leading-4">Ends in</p>
+                              <h2 class="text-lg font-bold">12h 43m 42s</h2>
+                         </div>
+                    </div>
+               </div>
+          </div>
      </div>
+     <!-- Banner End -->
+     <!-- Featured Section Start -->
+     <div class="bg-primaryGray bg-opacity-20 commonSpacing flex justify-between items-start gap-x-[50px] py-24">
+          <div>
+               <h2 class="text-[28px] leading-[34px] font-bold uppercase">The amazing NFT art of the world here</h2>
+          </div>
+          <div class="flex justify-start items-start gap-x-4">
+               <img src="@/assets/images/icons/card-tick-icon.svg" alt="">
+               <div>
+                    <h2 class="text-xl leading-[26px] font-bold">Fast Transaction</h2>
+                    <p class="text-sm leading-[22px] font-normal text-lightText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam etiam viverra tellus imperdiet.</p>
+               </div>
+          </div>
+          <div class="flex justify-start items-start gap-x-4">
+               <img src="@/assets/images/icons/chart-square-icon.svg" alt="">
+               <div>
+                    <h2 class="text-xl leading-[26px] font-bold">Growth Transaction</h2>
+                    <p class="text-sm leading-[22px] font-normal text-lightText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam etiam viverra tellus</p>
+               </div>
+          </div>
+     </div>
+     <!-- Featured Section End -->
+     <!-- Top Collection Section Start -->
+     <div class="commonSpacing flex justify-between items-start divide-x py-[130px]">
+          <div class="w-[70%] flex justify-start gap-x-4">
+               <div class="w-1/2">
+                    <div class="">
+                         <img src="@/assets/images/cardsimg/card-1.svg" alt="" />
+                         <div class="mt-2 flex w-full items-center justify-between">
+                              <div class="w-[235px]">
+                                   <div class="mt-2 flex items-center justify-start gap-x-3">
+                                        <img class="w-8" src="@/assets/images/avatar/avatar-2.svg" alt="" />
+                                        <div>
+                                             <p class="text-xl font-bold">The Futr Abstr</p>
+                                             <p class="text-sm font-bold">10 in the stock</p>
+                                        </div>
+                                   </div>
+                              </div>
+                              <div>
+                                   <p class="text-base font-medium leading-4">Highest Bid</p>
+                                   <div class="flex items-center justify-start gap-x-2">
+                                        <img class="w-2" src="@/assets/images/icons/grayRombosIcon.svg" alt="" />
+                                        <h2 class="text-lg font-bold">0.25 ETH</h2>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+               <div class="w-1/2 flex flex-col gap-y-4">
+                    <div class="flex items-center justify-start gap-x-5">
+                         <img src="@/assets/images/cardsimg/card-2.svg" alt="" />
+                         <div class="">
+                              <p class="text-xl font-bold">The Futr Abstr</p>
+                              <div class="mt-2 flex items-center justify-start gap-x-3">
+                                   <img class="w-8" src="@/assets/images/avatar/humanAvatar-1.svg" alt="" />
+                                   <div class="flex items-center justify-start gap-x-2 rounded-md border-2 border-green-600 p-2 text-sm text-green-600">
+                                        <img class="w-2" src="@/assets/images/icons/greenRombosIcon.svg" alt="" />
+                                        <p class="text-xs font-bold">0.25 ETH</p>
+                                   </div>
+                                   <p class="text-gray-600">1 of 8</p>
+                              </div>
+                              <button class="w-3/5 mt-2 rounded-full border-2 border-blue-800 py-2 px-3 text-sm font-normal text-blue-800 transition duration-200 ease-in hover:bg-blue-800 hover:text-white">Place a bid</button>
+                         </div>
+                    </div>
+                    <div class="flex items-center justify-start gap-x-5">
+                         <img src="@/assets/images/cardsimg/card-3.svg" alt="" />
+                         <div class="">
+                              <p class="text-xl font-bold">The Futr Abstr</p>
+                              <div class="mt-2 flex items-center justify-start gap-x-3">
+                                   <img class="w-8" src="@/assets/images/avatar/humanAvatar-1.svg" alt="" />
+                                   <div class="flex items-center justify-start gap-x-2 rounded-md border-2 border-green-600 p-2 text-sm text-green-600">
+                                        <img class="w-2" src="@/assets/images/icons/greenRombosIcon.svg" alt="" />
+                                        <p class="text-xs font-bold">0.25 ETH</p>
+                                   </div>
+                                   <p class="text-gray-600">1 of 8</p>
+                              </div>
+                              <button class="w-3/5 mt-2 rounded-full border-2 border-blue-800 py-2 px-3 text-sm font-normal text-blue-800 transition duration-200 ease-in hover:bg-blue-800 hover:text-white">Place a bid</button>
+                         </div>
+                    </div>
+                    <div class="flex items-center justify-start gap-x-5">
+                         <img src="@/assets/images/cardsimg/card-4.svg" alt="" />
+                         <div class="">
+                              <p class="text-xl font-bold">The Futr Abstr</p>
+                              <div class="mt-2 flex items-center justify-start gap-x-3">
+                                   <img class="w-8" src="@/assets/images/avatar/humanAvatar-1.svg" alt="" />
+                                   <div class="flex items-center justify-start gap-x-2 rounded-md border-2 border-green-600 p-2 text-sm text-green-600">
+                                        <img class="w-2" src="@/assets/images/icons/greenRombosIcon.svg" alt="" />
+                                        <p class="text-xs font-bold">0.25 ETH</p>
+                                   </div>
+                                   <p class="text-gray-600">1 of 8</p>
+                              </div>
+                              <button class="w-3/5 mt-2 rounded-full border-2 border-blue-800 py-2 px-3 text-sm font-normal text-blue-800 transition duration-200 ease-in hover:bg-blue-800 hover:text-white">Place a bid</button>
+                         </div>
+                    </div>
+               </div>
+          </div>
+          <div class="w-[30%] pl-3">
+               <h2 class="text-xl leading-6 font-bold">Top Collections over</h2>
+               <p class="mt-2 text-lg leading-[23px] font-bold text-primaryBlue">Last 7 days</p>
+
+               <div class="mt-8 flex flex-col gap-y-3 divide-y">
+                    <div class="flex items-center justify-start gap-x-2">
+                         <span>1</span>
+                         <div class="relative w-full">
+                              <img class="w-4/5" src="@/assets/images/avatar/avatar-1.svg" alt="" />
+                              <img class="absolute top-0 left-12 z-10 w-2/5" src="@/assets/images/icons/verify-icon.svg" alt="" />
+                         </div>
+                         <div class="w-full">
+                              <p class="text-base font-medium leading-4">Current Bid</p>
+                              <div class="flex items-center justify-start gap-x-2">
+                                   <img class="w-2" src="@/assets/images/icons/blackRombosIcon.svg" alt="" />
+                                   <h2 class="text-lg font-bold">0.25 ETH</h2>
+                              </div>
+                         </div>
+                         <p class="font-semibold text-[#14C8B0]">+26.52%</p>
+                    </div>
+                    <div class="flex items-center justify-start gap-x-2 pt-3">
+                         <span>2</span>
+                         <div class="relative w-full">
+                              <img class="w-4/5" src="@/assets/images/avatar/avatar-2.svg" alt="" />
+                              <!-- <img class="absolute top-0 left-12 z-10 w-2/5" src="@/assets/images/icons/verify-icon.svg" alt="" /> -->
+                         </div>
+                         <div class="w-full">
+                              <p class="text-base font-medium leading-4">Current Bid</p>
+                              <div class="flex items-center justify-start gap-x-2">
+                                   <img class="w-2" src="@/assets/images/icons/blackRombosIcon.svg" alt="" />
+                                   <h2 class="text-lg font-bold">0.25 ETH</h2>
+                              </div>
+                         </div>
+                         <p class="font-semibold text-[#14C8B0]">+26.52%</p>
+                    </div>
+                    <div class="flex items-center justify-start gap-x-2 pt-3">
+                         <span>3</span>
+                         <div class="relative w-full">
+                              <img class="w-4/5" src="@/assets/images/avatar/avatar-3.svg" alt="" />
+                              <!-- <img class="absolute top-0 left-12 z-10 w-2/5" src="@/assets/images/icons/verify-icon.svg" alt="" /> -->
+                         </div>
+                         <div class="w-full">
+                              <p class="text-base font-medium leading-4">Current Bid</p>
+                              <div class="flex items-center justify-start gap-x-2">
+                                   <img class="w-2" src="@/assets/images/icons/blackRombosIcon.svg" alt="" />
+                                   <h2 class="text-lg font-bold">0.25 ETH</h2>
+                              </div>
+                         </div>
+                         <p class="font-semibold text-[#14C8B0]">+26.52%</p>
+                    </div>
+                    <div class="flex items-center justify-start gap-x-2 pt-3">
+                         <span>4</span>
+                         <div class="relative w-full">
+                              <img class="w-4/5" src="@/assets/images/avatar/avatar-4.svg" alt="" />
+                              <!-- <img class="absolute top-0 left-12 z-10 w-2/5" src="@/assets/images/icons/verify-icon.svg" alt="" /> -->
+                         </div>
+                         <div class="w-full">
+                              <p class="text-base font-medium leading-4">Current Bid</p>
+                              <div class="flex items-center justify-start gap-x-2">
+                                   <img class="w-2" src="@/assets/images/icons/blackRombosIcon.svg" alt="" />
+                                   <h2 class="text-lg font-bold">0.25 ETH</h2>
+                              </div>
+                         </div>
+                         <p class="font-semibold text-[#14C8B0]">+26.52%</p>
+                    </div>
+                    <div class="flex items-center justify-start gap-x-2 pt-3">
+                         <span>5</span>
+                         <div class="relative w-full">
+                              <img class="w-4/5" src="@/assets/images/avatar/avatar-5.svg" alt="" />
+                              <img class="absolute top-0 left-12 z-10 w-2/5" src="@/assets/images/icons/verify-icon.svg" alt="" />
+                         </div>
+                         <div class="w-full">
+                              <p class="text-base font-medium leading-4">Current Bid</p>
+                              <div class="flex items-center justify-start gap-x-2">
+                                   <img class="w-2" src="@/assets/images/icons/blackRombosIcon.svg" alt="" />
+                                   <h2 class="text-lg font-bold">0.25 ETH</h2>
+                              </div>
+                         </div>
+                         <p class="font-semibold text-[#14C8B0]">+26.52%</p>
+                    </div>
+               </div>
+          </div>
+     </div>
+     <!-- Top Collection Section End -->
 </template>
 
 <script>
